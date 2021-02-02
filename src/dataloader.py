a5758c530a9ae7b379b26a940fa9daff458dccce
Atsuhiro Imagawa <a_imagawa@gpu01.ripes.jp>
2021-02-02 17:09:40 +0900

    train

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 import numpy as np
+import datetime as dt
+
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -13,7 +15,7 @@ class dataloader():
         self.img_size = img_size
         
     def preprocess(self, image):
-        image = tf.image.resize(image, [self.img_size*2, self.img_size*2])
+        image = tf.image.resize(image, [int(self.img_size*1.5), int(self.img_size*1.5)])
         image = tf.cast(image, tf.float16)
         image = rescale(image)
         return image
@@ -60,17 +62,16 @@ class aug_dataloader():
         self.batch_size = batch_size
         self.img_size = img_size
         
-    def rand_float(self, _min, _max):
-        return (np.random.random_sample()/(_max-_min))-_min
-        
     def preprocess(self, image):
-        image = tf.image.resize(image, [self.img_size*2, self.img_size*2])
+        image = tf.image.resize(image, [int(self.img_size*1.5), int(self.img_size*1.5)])
         image = tf.cast(image, tf.float16)
         image = rescale(image)
-        image = tfa.image.translate_xy(image, [self.rand_float(-0.2, 0.3), self.rand_float(-0.2, 0.3)], 0)
-        image = tfa.image.rotate(image, self.rand_float(-1, 1))
-        image = tf.image.random_crop(image, [self.img_size, self.img_size, 3])
-        image = tf.image.random_contrast(image, 0.2, 0.3)
+        
+        image = tfa.image.translate_xy(image, tf.random.uniform([2], minval=-0.2, maxval=0.3), 0)
+        image = tfa.image.rotate(image, tf.random.uniform([1], minval=-3.14, maxval=3.14),)
+        
+        image = tf.image.random_crop(image, [self.img_size, self.img_size, 3], seed=int(dt.datetime.now().timestamp()))
+        image = tf.image.random_contrast(image, 0.5, 1.3, seed=int(dt.datetime.now().timestamp()))
         return image
 
     def load_img(self, path):
